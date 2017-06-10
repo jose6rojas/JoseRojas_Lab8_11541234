f45de25c2a9141044427782bf316d87d7a121414
Jose Rojas <jose23rojas@unitec.edu>
2017-06-09 20:53:41 -0600

    losing hope

--- Lab8.cpp
+++ Lab8.cpp
@@ -13,6 +13,8 @@
 
 int main(int argc, char const *argv[])
 {
+	vector<Real*> reales;
+
 	int opcion = 0;
 	while(opcion != 3)
 	{
@@ -33,20 +35,35 @@ int main(int argc, char const *argv[])
 				cout << "2. Radical" << endl;
 				cout << "3. Salir" << endl;
 				cout << "Escoja una opcion: ";
+				cin >> opcion1;
 
-				if(opcion1 == 1)
+				if(opcion1 == 1) // Racional
 				{
 					int num, den;
 					cout << endl << "Ingrese el numerador: ";
 					cin >> num;
 					cout << "Ingrese el denominador: ";
 					cin >> den;
-				}
-				else if(opcion1 == 2)
-				{
 
+					Racional* R = new Racional(num, den);
+					reales.push_back(R);
+					cout << endl << "Numero Racional agregado correctamente." << endl;
 				}
-				else if(opcion1 == 3) { }
+				else if(opcion1 == 2) // Radical
+				{
+					int coeficiente, indice, radicando;
+					cout << endl << "Ingrese el coeficiente: ";
+					cin >> coeficiente;
+					cout << "Ingrese el indice: ";
+					cin >> indice;
+					cout << "Ingrese el radicando: ";
+					cin >> radicando;
+
+					Radical* R = new Radical(coeficiente, indice, radicando);
+					reales.push_back(R);
+					cout << endl << "Numero Radical agregado correctamente." << endl;
+				}
+				else if(opcion1 == 3) { cout << endl; }
 				else
 				{
 					cout << endl << "Debe ingresar una opcion valida." << endl;
@@ -56,28 +73,124 @@ int main(int argc, char const *argv[])
 
 		else if(opcion == 2) // Operar Numeros Reales
 		{
-			int opcion2 = 0;
-			while(opcion2 != 3)
+			cout << endl << "Reales en el banco" << endl;
+			for(int i = 0; i < reales.size(); i++)
 			{
-				cout << endl << "Operar Numeros Reales" << endl;
-				cout << "1. Racional" << endl;
-				cout << "2. Radical" << endl;
-				cout << "3. Salir" << endl;
-				cout << "Escoja una opcion: ";
-
-				if(opcion2 == 1)
+				if(dynamic_cast<Racional*> (reales[i]))
 				{
-
+					int num, den;
+					num = (dynamic_cast<Racional*> (reales[i])) -> getNumerador();
+					den = (dynamic_cast<Racional*> (reales[i])) -> getDenominador();
+					cout << i << ". " << "Racional -> " << num << "/" << den << endl;
 				}
-				else if(opcion2 == 2)
-				{
-
-				}
-				else if(opcion2 == 3) { }
 				else
 				{
-					cout << endl << "Debe ingresar una opcion valida." << endl;
+					int coef, indice, radicando;
+					coef = (dynamic_cast<Radical*> (reales[i])) -> getCoeficiente();
+					indice = (dynamic_cast<Radical*> (reales[i])) -> getIndice();
+					radicando = (dynamic_cast<Radical*> (reales[i])) -> getRadicando();
+					cout << i << ". " << "Radical -> " << "(" << coef << ")(" << radicando << ")^(1/" << indice << ")" 
+					<< endl;
 				}
+			}
+
+			int op;
+			cout << "Escoja un numero Real: ";
+			cin >> op;
+
+			Racional* Rac;
+			Radical* Rad;
+
+			if(dynamic_cast<Racional*> (reales[op]))
+			{
+				cout << endl << "Escogio un Racional" << endl;
+				Rac = dynamic_cast<Racional*> (reales[op]);
+
+				int opRac;
+				cout << endl << "Operando un Racional" << endl;
+				cout << "1. Racional" << endl;
+				cout << "2. Radical" << endl;
+				cout << "3. Entero" << endl;
+				cout << "4. Decimal" << endl;
+				cout << "Escoja una opcion: ";
+				cin >> opRac;
+
+				if(opRac == 1)
+				{
+					cout << endl << "Racionales en el banco" << endl;
+					for(int i = 0; i < reales.size(); i++)
+					{
+						if(dynamic_cast<Racional*> (reales[i]))
+						{
+							int num, den;
+							num = (dynamic_cast<Racional*> (reales[i])) -> getNumerador();
+							den = (dynamic_cast<Racional*> (reales[i])) -> getDenominador();
+							cout << i << ". " << "Racional -> " << num << "/" << den << endl;
+						}
+					}
+
+					int op2;
+					cout << "Escoja un numero Real: ";
+					cin >> op2;
+
+					Racional* Rac2;
+					Rac2 = dynamic_cast<Racional*> (reales[op2]);
+
+					int operacion;
+					cout << endl << "Racional y Racional" << endl;
+					cout << "1. Suma" << endl;
+					cout << "2. Resta" << endl;
+					cout << "3. Multiplicacion" << endl;
+					cout << "4. Division" << endl;
+					cout << "Escoja una operacion: ";
+					cin >> operacion;
+
+					string resultado;
+					if(operacion == 1) // Suma
+					{
+						resultado = *Rac + Rac2;
+						cout << endl << resultado << endl;
+					}
+					else if(operacion == 2) // Resta
+					{
+						resultado = *Rac - Rac2;
+						cout << endl << resultado << endl;
+					}
+					else if(operacion == 3) // Multiplicacion
+					{
+						resultado = *Rac * (Rac2);
+						cout << endl << resultado << endl;
+					}
+					else if(operacion == 4) // Division
+					{
+						resultado = *Rac / Rac2;
+						cout << endl << resultado << endl;
+					}
+					else
+						cout << endl << "Debe ingresar una opcion valida." << endl;
+				}
+				else if(opRac == 2)
+				{
+
+				}
+				else if(opRac == 3)
+				{
+
+				}
+				else if(opRac == 4)
+				{
+
+				}
+				else
+					cout << endl << "Debe escoger una opcion valida." << endl;
+			}
+			else
+			{
+				cout << endl << "Escogio un Radical" << endl;
+				Rad = dynamic_cast<Radical*> (reales[op]);
+
+
+			}
 		}
 
 		else if(opcion == 3) { }
@@ -86,6 +199,11 @@ int main(int argc, char const *argv[])
 			cout << endl << "Debe ingresar una opcion valida." << endl;
 		}
 	}
+
+	// Limpia el vector de Reales
+	for (int i = 0; i < reales.size(); i++)
+		delete reales[i];
+	reales.clear();
 
 	return 0;
 }
